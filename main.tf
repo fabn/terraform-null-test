6a23bd8d479300143d51b88d3eb59ce5fcca531e
Fabio Napoleoni <f.napoleoni@gmail.com>
2025-05-10 12:59:26 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "example"
+  tags = {
+    Project     = "naming-test"
+  }
+}
